c3fa30f74406f5f9149eeab45f52525c161ce1d2
MuhammadEsmaiel <47538199+MuhammadEsmaiel@users.noreply.github.com>
2022-11-20 10:58:09 +0200

    handle my database

--- db/migrate/20221120001836_creat_application.rb
+++ db/migrate/20221120001836_creat_application.rb
@@ -1,11 +1,10 @@
-class CreateApplications < ActiveRecord::Migration[7.0]
+class CreatApplication < ActiveRecord::Migration[7.0]
   def change
     create_table :applications do |t|
       t.string :token
       t.string :name
       t.integer :chat_no
       t.integer :msg_no
-      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
