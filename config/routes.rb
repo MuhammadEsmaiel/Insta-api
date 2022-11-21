f9119977771b813c4c202c48b849fa67f4615b55
MuhammadEsmaiel <47538199+MuhammadEsmaiel@users.noreply.github.com>
2022-11-21 19:00:34 +0200

    some spices

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-      resources :users do
         resources :applications do
-          resources :chats do
-            resources :messages 
+          resources :chat do
+            resources :message 
           end
         end
-      end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #, only: [:index,:show,:create]
   # Defines the root path route ("/")
